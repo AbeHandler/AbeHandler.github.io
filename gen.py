e8210bd053454830f7c4a73349b930803b0c871e
akh2103 <akh2103@gmail.com>
2014-04-05 10:04:26 -0400

    aquiii

--- gen.py
+++ gen.py
@@ -5,6 +5,7 @@ import random
 
 featuredict = []
 types = ["nora", "sheriff", "demo", "fixed"]
+investigates = ["f", "f", "f", "f", "t", "f"]
 
 for x in range(0, 7500):
 	feature = dict()
@@ -13,7 +14,7 @@ for x in range(0, 7500):
 	geom["type"] = "Point"
 	geom["coordinates"] = [random.uniform(-89.9, -90.12),random.uniform(29.9, 30.05)]
 	feature["geometry"] = geom
-	feature["properties"] = dict(category=types[randint(0,3)])
+	feature["properties"] = dict(category=types[randint(0,3)],investigates=investigates[randint(0,5)])
 	featuredict.append(feature)
 
 type = dict(type="FeatureCollection", features = featuredict)
