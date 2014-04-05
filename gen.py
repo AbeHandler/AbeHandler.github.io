0b494b4f9b0bb91d836f19b07d30ff1933922246
akh2103 <akh2103@gmail.com>
2014-04-05 00:12:33 -0400

    getting closer

---
+++ gen.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+from random import randint
+import json
+import random
+
+featuredict = []
+types = ["nora", "sheriff", "demo", "fixed"]
+
+for x in range(0, 7500):
+	feature = dict()
+	geom = dict()
+	feature["type"] = "Feature"
+	geom["type"] = "Point"
+	geom["coordinates"] = [random.uniform(-89.9, -90.12),random.uniform(29.9, 30.05)]
+	feature["geometry"] = geom
+	feature["properties"] = dict(category=types[randint(0,3)])
+	featuredict.append(feature)
+
+type = dict(type="FeatureCollection", features = featuredict)
+f = open("out.json", "w")
+print(json.dumps(type), file=f)
