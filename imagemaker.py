d5dc89f73e6a2f0ba4fd9366e6fbcbf23b13ab3f
akh2103 <akh2103@gmail.com>
2014-04-10 23:20:43 -0500

    heee

--- imagemaker.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-image = Image.open('static/images/4719_Redwood.jpg')
-image.thumbnail((200, 200), Image.ANTIALIAS)
-image.save('static/images/123_Octavia_St.jpg', 'JPEG', quality=88)
