0753197a38eed9c5ddd2b3a3f3831369da22c151
akh2103 <akh2103@gmail.com>
2014-04-11 12:47:42 -0500

    heee

--- getcharlesdata.py
+++ getcharlesdata.py
@@ -8,16 +8,26 @@ gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-with open('VisitingPhotos.csv', 'rU') as csvfile:
-	for row in csv.reader(csvfile, delimiter=','):
-		try:
-			address = row[2].replace(' ', '_')
-			links = [i for i in row[3].split(" ") if len(i)>0] 
-			for l in links:
-				myfile = drive.CreateFile({'id': l.split("=")[2]})
-				for property, value in vars(myfile).iteritems():
-					print property, ": ", value
-				if not os.path.exists('static/images/big/' + address + ".jpg"):
-					myfile.GetContentFile('static/images/big/' + address + ".jpg")
-		except: 
-			print row
+with open('Visited4.csv', 'rU') as csvfile:
+    for row in csv.reader(csvfile, delimiter=','):
+        try:
+            address = row[2].replace(' ', '_')
+            neighborhod = row[4]
+            type = row[6]
+            notes = row[15]
+            owner = row[7]
+            ownerlink = row[5]
+            caption = address.replace("_", "") + "," +neighborhod + "<br>" + "History:"
+            if len(notes)==0:
+                caption = "<br>" + caption + "Notes:" + notes
+            with open("static/captions/" + address,"a+") as f:
+                f.write(caption)
+			#links = [i for i in row[3].split(" ") if len(i)>0]
+			#for l in links:
+			#	myfile = drive.CreateFile({'id': l.split("=")[2]})
+			#	for property, value in vars(myfile).iteritems():
+			#		print property, ": ", value
+			#	if not os.path.exists('static/images/big/' + address + ".jpg"):
+			#		myfile.GetContentFile('static/images/big/' + address + ".jpg")
+        except:
+            print row
